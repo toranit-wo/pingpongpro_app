2a26d167344eedfe3f609238e6e8c1d23dbdedc2
toranit-wo <toranit.wo.60@ubu.ac.th>
2021-06-11 17:37:12 +0700

    'send'

--- frontend/pingpongpro_app/lib/main.dart
+++ frontend/pingpongpro_app/lib/main.dart
@@ -20,16 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-///      MaterialApp(
-///      title: 'Fitness App',
-///      theme: ThemeData(
-///        primarySwatch: Colors.blue,
-///        primaryColor: Color(0XFF6D3FFF),
-///        accentColor: Color(0XFF233C63),
-///        fontFamily: 'Poppins',
-///      ),
-///      home: Welcome(),
-///    );
-///  }
-///}
